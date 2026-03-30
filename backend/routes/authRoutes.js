578fcbf50d1535be0decf048258664d673d6ed37
thomxz-cod <thomazerick35@gmail.com>
2026-03-30 15:34:04 -0300

    Update

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,13 @@
+import express from "express"
+
+const router = express.Router();
+
+router.get("/register", async (req, res)=> {
+    res.send("register");
+})
+
+router.get("/login", async (req, res)=> {
+    res.send("login");
+})
+
+export default router
